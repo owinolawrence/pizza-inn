ca8a93b4629859314256e0932f98f1d1bec39ec3
owinolawrence <owinolawrence21@gmail.com>
2019-10-05 10:27:19 +0300

    Add README.md file

---
+++ script.js
@@ -0,0 +1,10 @@
+// $(document).ready(function(){
+// var size= $("#size").find(":selected").text();
+// $(button).click(function(){
+// $ ("button").show('#crust') 
+// });
+// }
+function get_amount(){
+var x = document.getElementById("Select").selectedIndex;
+alert(x);
+}
